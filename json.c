394caf841226932ec1073f6871b182c9153b9f6c
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-12-02 08:45:48 +0500

    fix: Make empty array test pass

--- json.c
+++ json.c
@@ -209,6 +209,8 @@ consume_array(Scanner *scanner)
         return 1;
     }
 
+    consume_spaces(scanner);
+
     if (scanner->data[scanner->pos] != ']') {
         do {
             if (consume_json_value(scanner)) {
